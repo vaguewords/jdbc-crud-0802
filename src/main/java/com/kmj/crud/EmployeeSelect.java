890152d06e9267894eaa51dfc0ef7e2dd97dc65a
vaguewords <59818937+vaguewords@users.noreply.github.com>
2023-08-03 10:18:03 +0900

    Update EmployeeSelect.java

--- src/main/java/com/kmj/crud/EmployeeSelect.java
+++ src/main/java/com/kmj/crud/EmployeeSelect.java
@@ -6,8 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.util.Scanner;
+import java.util.*;
 
 
 import static com.kmj.common.JDBCConnect.close;
@@ -38,12 +37,33 @@ public class EmployeeSelect {
             rset = pstmt.executeQuery();
             System.out.println(deptCode + "번 부서 직원 목록은 다음과 같습니다.");
             while(rset.next()) {
-                String empName = rset.getString("EMP_NAME");
+                /*String empName = rset.getString("EMP_NAME");
                 String empId = rset.getString("EMP_ID");
                 String email = rset.getString("EMAIL");
 
-                System.out.println("ID: " + empId + " / 이름: " + empName + " / Contact: " + email);
+                System.out.println("ID: " + empId + " / 이름: " + empName + " / Contact: " + email);*/
+
+                List<Map<String, String>> employeeList = new ArrayList<Map<String, String>>();
+                while (rset.next()) {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("empId", rset.getString("EMP_ID"));
+                    map.put("empName", rset.getString("EMP_Name"));
+                    map.put("email", rset.getString("email"));
+                    employeeList.add(map);
+                }
+
+
+                for (int i = 0; i < employeeList.size(); i++) {
+                    System.out.println("아이디: " + employeeList.get(i).get("empId"));
+                    System.out.println("이름: " +  employeeList.get(i).get("empName"));
+                    System.out.println("email: " +  employeeList.get(i).get("email"));
+                    System.out.println("===");
+                }
+
             }
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (SQLException e) {
